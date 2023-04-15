83a8ef4d9f52ead927075136998e12223262535b
Lohit <lohitkbisen@gmail.com>
2023-04-15 20:02:37 +0530

    Removed aspect

--- src/main/java/club/blink/aspects/NameServiceAspect.java
+++ src/main/java/club/blink/aspects/NameServiceAspect.java
@@ -9,14 +9,14 @@ import org.springframework.stereotype.Component;
 @Aspect
 public class NameServiceAspect {
 
-	@Around("execution(* club.blink.controller.NameServiceController.getName(..))")
-	private Object getNameJoinPoint(ProceedingJoinPoint pjp) throws Throwable {
-		System.out.println("GET NAME CALLED");
-		System.out.println(pjp.getTarget().getClass());
-		System.out.println(String.valueOf(pjp.getArgs()));
-
-		Object retVal = pjp.proceed();
-		System.out.println("GET NAME Returned");
-		return retVal;
-	}
+//	@Around("execution(* club.blink.controller.NameServiceController.getName(..))")
+//	private Object getNameJoinPoint(ProceedingJoinPoint pjp) throws Throwable {
+//		System.out.println("GET NAME CALLED");
+//		System.out.println(pjp.getTarget().getClass());
+//		System.out.println(String.valueOf(pjp.getArgs()));
+//
+//		Object retVal = pjp.proceed();
+//		System.out.println("GET NAME Returned");
+//		return retVal;
+//	}
 }
