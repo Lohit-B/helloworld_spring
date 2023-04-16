19a8e91cdd0b18bb3d74af2782864d65322da075
Lohit <lohitkbisen@gmail.com>
2023-04-16 12:48:57 +0530

    Basic homepage

--- src/main/java/club/blink/controller/NameServiceController.java
+++ src/main/java/club/blink/controller/NameServiceController.java
@@ -1,17 +1,15 @@
 package club.blink.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class NameServiceController {
 	
 	 @GetMapping("/")
-	 public ResponseEntity<String> getName() {
+	 public String getName() {
 		
-		 return new ResponseEntity<String>("Coming Soon", HttpStatus.OK);
+		 return "home";
 	 }
 	
 }	
